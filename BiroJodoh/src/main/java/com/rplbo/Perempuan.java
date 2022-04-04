daf0c95c684874d47ebae90e7ab6d5ec5df874ed
71200675sabrina <79124326+71200675sabrina@users.noreply.github.com>
2022-04-04 16:23:47 +0700

    Add files via upload

---
+++ BiroJodoh/src/main/java/com/rplbo/Perempuan.java
@@ -0,0 +1,15 @@
+package com.rplbo;
+
+public class Perempuan {
+    private String nama;
+    private int usia;
+    private String status;
+    private Double jumlahCinta;
+    private Pria jodoh;
+    private Pria temanKencan;
+    private Double standarJumlahCinta;
+
+    public void MengajakKencan(Pria pria){
+
+    }
+}
---
+++ BiroJodoh/src/main/java/com/rplbo/Pria.java
@@ -0,0 +1,21 @@
+package com.rplbo;
+
+public class Pria {
+    private String nama;
+    private int usia;
+    private String status;
+    private Double jumlahCinta;
+    private Perempuan jodoh;
+    private Perempuan temanKencan;
+
+    public void PDKT(Perempuan calon){
+
+    }
+
+    public void MengajakKencan(Perempuan perempuan){
+
+    }
+
+    public void MenembakKekasih(){}
+
+}
