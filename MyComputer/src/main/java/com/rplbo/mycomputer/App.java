b738394c5a40036b231bd4f8e405f59123a5a814
71200675sabrina <79124326+71200675sabrina@users.noreply.github.com>
2022-04-04 16:24:49 +0700

    Add files via upload

---
+++ MyComputer/src/main/java/com/rplbo/mycomputer/App.java
@@ -0,0 +1,17 @@
+package com.rplbo.mycomputer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        public App(){}
+
+        public void main(String[]){}
+
+        public String checkBenar(boolean){}
+    }
+}
---
+++ MyComputer/src/main/java/com/rplbo/mycomputer/Laptop.java
@@ -0,0 +1,18 @@
+package com.rplbo.mycomputer;
+
+public class Laptop {
+    private Keyboard keyboard;
+    private Monitor monitor;
+
+    default Laptop(Storage,Monitor,Keyboard){
+
+    }
+
+    public void showDisplay(){
+
+    }
+
+    public void retrieveData(double){
+
+    }
+}
